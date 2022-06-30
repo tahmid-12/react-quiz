3d68196375785bb8bcc49487681eaea7ecc288f4
tahmid-12 <mdtahmidchowdhury12@gmail.com>
2022-06-30 23:37:53 +0600

    Tailwind installed, pages created.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import {
+  Routes,
+  Route,
+} from "react-router-dom";
+import Home from "./pages/Home";
+import Quiz from "./pages/Quiz";
+import Result from "./pages/Result";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="flex flex-col justify-center items-center h-screen">
+        <h1 className="font-mono font-extrabold text-5xl">React Quiz App</h1>
+        <p className="font-mono text-3xl font-medium">Welcome to the app Some-User</p>
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route path="/quiz" element={<Quiz />}/>
+          <Route path="/result" element={<Result />}/>
+        </Routes>
+      </div>
+    </>
   );
 }
 
