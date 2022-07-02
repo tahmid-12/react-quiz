0dbe33499cddbae8b50306acc8cb4378f5024ca4
tahmid-12 <mdtahmidchowdhury12@gmail.com>
2022-07-02 09:56:02 +0600

    Routing created and User name placed in local Storage.

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -11,23 +11,13 @@ function Home() {
 
   useEffect(() => {
 
-    // if (localStorage.getItem('Name') === null) {
-    //   const random = Math.floor(Math.random() * data.length);
-    //   localStorage.setItem('Name', data[random].name);
-    // }else{
-    //   const items = JSON.stringify(localStorage.getItem('Name'));
-    //   if(items){
-    //     setName(items)
-    //   }
-    // }
+    if (localStorage.getItem('Name') === null) {
+      const random = Math.floor(Math.random() * data.length);
+      localStorage.setItem('Name', data[random].name);
+    }
     const items = JSON.stringify(localStorage.getItem('Name'));
     if(items){
         setName(items)
-    }else{
-      const random = Math.floor(Math.random() * data.length);
-      localStorage.setItem('Name', data[random].name);
-      const val = JSON.stringify(localStorage.getItem('Name'));
-      setName(val)
     }
   },[]);
 
@@ -60,7 +50,7 @@ function Home() {
 
   return (
     <div className="flex flex-col justify-center items-center py-5">
-      <p className="font-mono text-3xl font-medium">Welcome to the app {}</p>
+      <p className="font-mono text-3xl font-medium">Welcome to the app {name}</p>
       <p className="font-mono text-xl font-medium">Click on the button or press 'Enter' from your keyboard to start the test</p>
         <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold mt-5 py-2 px-4 border border-blue-700 rounded" 
                 onClick={() => handleSubmit()}
